5ba8a2d3a4865f6d97dfa6775de7fa2b8848ba99
vittayang <vittayang@gmail.com>
2015-04-20 16:35:22 +0800

    Create Monad.v

---
+++ Monad.v
@@ -0,0 +1,24 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(* Verified Flash Translation Layer                                           *)
+(*                                                                            *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+Notation "'do' x <-- A ; B" := 
+  (match A with 
+     | Some y => (fun x => B) y 
+     | None => None end)
+  (at level 200, x ident).
+
+Notation "'do' '[' x1 ',' x2 ']' <-- o ; f" :=
+  (match o with Some (y1, y2) => (fun x1 x2 => f) y1 y2 | None => None end)
+  (at level 200).
+
+Notation "'test' b ; f" :=
+  (match b with false => None | true => f end)
+  (at level 200).
+
+Notation "'ret' x" := (Some x) (at level 200, only parsing).
+
+
